769c4232346735f88b029290017d1e0588cffd62
safwatnusrat <safwatn89@gmail.com>
2024-09-07 19:47:39 +0600

    initial commit

---
+++ app/src/main/java/com/example/assignment/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.assignment;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textview=findViewById(R.id.textView);
+        ImageView imageView = findViewById(R.id.imageView);
+        Button button = findViewById(R.id.signinbutton);
+        Button button2=findViewById(R.id.signupbutton);
+
+
+        button.setOnClickListener(v -> {
+            Toast.makeText(this, "Sign In is Clicked", Toast.LENGTH_SHORT).show();
+        });
+        button2.setOnClickListener(v->{
+            Toast.makeText(this, "Sign Up is Clicked", Toast.LENGTH_SHORT).show();
+        });
+    }
+}
